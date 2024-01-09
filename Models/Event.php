2997937135b11a7a408d7e4837b61cb8fa1a0fdd
marco76tv <marco76tv@users.noreply.github.com>
2024-01-09 07:57:41 +0000

    Check & fix styling

--- Models/Event.php
+++ Models/Event.php
@@ -25,11 +25,11 @@ use Modules\Lang\Models\Post;
  * @property string|null                                $created_by
  * @property string|null                                $updated_by
  * @property string|null                                $deleted_by
-<<<<<<< HEAD
+ *                                                                    <<<<<<< HEAD
  * @property Place|null                                 $place
-=======
- * @property \Modules\Geo\Models\Place|null             $place
->>>>>>> dev
+ *                                                                    =======
+ * @property Place|null                                 $place
+ *                                                                    >>>>>>> dev
  * @property Collection<int, \Modules\Geo\Models\Place> $places
  * @property int|null                                   $places_count
  *
