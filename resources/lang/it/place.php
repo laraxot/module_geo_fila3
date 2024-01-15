8193a691b7df10a21ac9b3ca8fb02ed66476b8ae
Retype GitHub Action <retype@object.net>
2024-01-15 17:36:07 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/lang/it/place.php
+++ resources/lang/it/place.php
@@ -3,14 +3,14 @@
 declare(strict_types=1);
 
 return [
-    'fields'=>[
-        'route'=>'Via/Piazza',
-        'street_number'=>'Numero civico',
-        'postal_code'=>'Cap',
-        'locality'=>'Città',
-        'administrative_area_level_2' =>'Provincia',
-        'administrative_area_level_1' =>'Regione',
-        'country'=>'Nazione',
+    'fields' => [
+        'route' => 'Via/Piazza',
+        'street_number' => 'Numero civico',
+        'postal_code' => 'Cap',
+        'locality' => 'Città',
+        'administrative_area_level_2' => 'Provincia',
+        'administrative_area_level_1' => 'Regione',
+        'country' => 'Nazione',
     ],
     'tab' => [
         'index' => 'Lista',
