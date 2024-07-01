7de663cfe754fdeef3c1f0780b073607d6c7e27f
marco76tv <marco.sottana@gmail.com>
2024-07-01 22:19:20 +0200

    chore(map.blade.php): update Livewire components in map.blade.php for better functionality and clarity docs: remove unnecessary content from various documentation files and configurations
    docs(readme.md): remove Jigsaw Docs Starter Template readme file and related assets
    docs(404.blade.php, _assets/css/main.css, _assets/js/main.js, _assets/sass/_base.scss, _assets/sass/_documentation.scss, _assets/sass/_navigation.scss): delete unused documentation files and assets to clean up the project.
    
    chore(docs): remove unused Sass files, category, components, and documentation pages
    
    chore(master.blade.php): remove unused master.blade.php layout file
    chore(post.blade.php): remove unused post.blade.php layout file
    chore(menu-item-tree.blade.php): remove unused menu-item-tree.blade.php file
    chore(menu-item.blade.php): remove unused menu-item.blade.php file
    chore(menu-toggle.blade.php): remove unused menu-toggle.blade.php file
    chore(menu-tree.blade.php): remove unused menu-tree.blade.php file
    chore(menu.blade.php): remove unused menu.blade.php file
    chore(search-input.blade.php): remove unused search-input.blade.php file
    
    docs(algolia-docsearch.md): remove Algolia DocSearch guide and related images
    docs(first_blog_post.md): remove first blog post
    docs(my-first-blog-post.md): remove my first blog post
    docs(icon-stack.svg): remove icon-stack.svg
    docs(icon-terminal.svg): remove icon-terminal.svg
    docs(icon-window.svg): remove icon-window.svg
    
    docs(assets): remove unused SVG files and logo images to clean up repository
    docs(blade): delete unused docs.blade.php file to declutter project structure
    docs(favicon): remove favicon.ico file to eliminate unnecessary files from the repo
    
    docs(index.blade.php): remove unnecessary content from the index page
    docs(links.txt): remove unused links file
    docs(posts.blade.php): remove posts page template
    docs(tailwind.config.js): remove tailwind configuration file
    docs(webpack.mix.js): remove webpack mix configuration file

--- Resources/views/components/blocks/map.blade.php
+++ Resources/views/components/blocks/map.blade.php
@@ -1,3 +1,9 @@
 <div>
-    MAPPONA
+    {{--
+    @livewire(\Modules\Geo\Filament\Widgets\LocationMapWidget::class)
+    --}}
+    @livewire(\Modules\Geo\Filament\Widgets\LocationMapTableWidget::class)
+    {{--
+    @livewire(\Modules\Blog\Filament\Widgets\SampleChartWidget::class)
+    --}}
 </div>
