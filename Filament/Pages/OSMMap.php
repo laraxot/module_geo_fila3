7d7cf172b48c2ee3929c850f90f9703f66a3d4bf
marco76tv <marco.sottana@gmail.com>
2024-07-02 11:14:04 +0200

    📝 (LocationMapTable.php): Update import statements for Widgets namespace in LocationMapTable.php for better organization and clarity 📝 (LocationMapTable.php): Update class references to use fully qualified class names under Widgets namespace in LocationMapTable.php ✨ (OSMMap.php): Add new OSMMap page to display OpenStreetMap with OSMMapWidget ✨ (OSMMapWidget.php): Create OSMMapWidget class to display OpenStreetMap with custom markers and actions 🔧 (composer.json): Add webbingbrasil/filament-maps dependency to composer.json for OSMMapWidget functionality

--- Filament/Pages/OSMMap.php
+++ Filament/Pages/OSMMap.php
@@ -1,11 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Geo\Filament\Pages;
 
-use Modules\Geo\Filament\Widgets;
 use Filament\Pages\Page;
 
-class LocationMapTable extends Page
+use Modules\Geo\Filament\Widgets;
+
+class OSMMap extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
@@ -14,8 +17,9 @@ class LocationMapTable extends Page
     protected function getHeaderWidgets(): array
     {
         return [
-            Widgets\LocationMapTableWidget::class,
+            // Widgets\LocationMapTableWidget::class,
             //Widgets\LocationMapWidget::class,
+            Widgets\OSMMapWidget::class,
         ];
     }
 
