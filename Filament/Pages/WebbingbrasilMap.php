c5833651e53dc87bf7288efb282de4c8502b44df
Malebestia <vair81@gmail.com>
2024-07-02 11:19:37 +0200

    rebase 1/1

---
+++ Filament/Pages/WebbingbrasilMap.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Modules\Geo\Filament\Pages;
+
+use Filament\Pages\Page;
+use Modules\Geo\Filament\Widgets\WebbingbrasilMap as Map;
+
+class WebbingbrasilMap extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'geo::filament.pages.webbingbrasil-map';
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            //LocationMapTableWidget::class,
+            Map::class,
+        ];
+    }
+
+    public function getHeaderWidgetsColumns(): int|array
+    {
+        return 1;
+    }
+}
