43b41843802e831d3fac209336b4dd38a05c9f33
marco76tv <marco76tv@users.noreply.github.com>
2024-07-27 09:36:21 +0000

    Check & fix styling

--- Models/County.php
+++ Models/County.php
@@ -7,11 +7,10 @@ namespace Modules\Geo\Models;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * 
- *
  * @method static \Illuminate\Database\Eloquent\Builder|County newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|County newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|County query()
+ *
  * @mixin \Eloquent
  */
 class County extends Model
--- Models/State.php
+++ Models/State.php
@@ -7,11 +7,10 @@ namespace Modules\Geo\Models;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * 
- *
  * @method static \Illuminate\Database\Eloquent\Builder|State newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|State newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|State query()
+ *
  * @mixin \Eloquent
  */
 class State extends Model
